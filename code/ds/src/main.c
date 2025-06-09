a4880486404ef65c84273f2c2fc9e94599e028dc
caoaolong <eason105cc@163.com>
2025-06-09 11:19:07 +0800

    双向链表 - 测试函数

--- code/ds/src/main.c
+++ code/ds/src/main.c
@@ -1,25 +1,24 @@
 ﻿#include <stdio.h>
-#include <sl_list.h>
+#include <dl_list.h>
 
 int main(void) {
     int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    sl_list *list = sl_list_create();
+    dl_list *list = dl_list_create();
     for (int i = 0; i < 10; i++) {
-        sl_node *node = sl_node_create(&array[i]);
-        sl_list_insert_front(list, node);
+        dl_node *node = dl_node_create(&array[i]);
+        dl_list_insert_front(list, node);
     }
 
     for (int i = 0; i < 10; i++) {
-        sl_node *node = sl_node_create(&array[i]);
-        sl_list_insert_back(list, node);
+        dl_node *node = dl_node_create(&array[i]);
+        dl_list_insert_back(list, node);
     }
 
-    sl_node *inode = sl_list_node(list, 3);
     int value = 20;
-    sl_node *node = sl_node_create(&value);
-    sl_list_insert(list, inode, node);
+    dl_node *node = dl_node_create(&value);
+    dl_list_insert(list, 3, node);
 
-    const sl_node *p = list->head;
+    const dl_node *p = list->head;
     while (p != NULL) {
         const int *data = p->data;
         printf("%d ", *data);
