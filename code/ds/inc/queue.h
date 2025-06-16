02cb43d52bf7082d2f9b291d372a2ecdbd2e42ad
caoaolong <eason105cc@163.com>
2025-06-16 16:03:11 +0800

    队列

---
+++ code/ds/inc/queue.h
@@ -0,0 +1,38 @@
+﻿//
+// Created by admin on 25-6-16.
+//
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <array.h>
+#include <dl_list.h>
+
+typedef array queue_array;
+
+// 队列（数组实现，前入后出）
+
+queue_array *queue_array_create();
+
+void queue_array_enqueue(queue_array *queue, void *data);
+
+void *queue_array_dequeue(queue_array *queue);
+
+void *queue_array_peek(const queue_array *queue);
+
+// 队列（双向链表实现）
+
+typedef dl_list queue_list;
+typedef dl_node queue_node;
+
+queue_list *queue_list_create();
+
+queue_node *queue_node_create(void *data);
+
+void queue_list_enqueue(queue_list *queue, queue_node *node);
+
+queue_node *queue_list_dequeue(queue_list *queue);
+
+queue_node *queue_list_peek(const queue_list *queue);
+
+#endif //QUEUE_H
---
+++ code/ds/src/queue.c
@@ -0,0 +1,36 @@
+﻿//
+// Created by admin on 25-6-16.
+//
+#include <queue.h>
+
+void queue_array_enqueue(queue_array *queue, void *data) {
+    return array_insert_front(queue, data);
+}
+
+void *queue_array_dequeue(queue_array *queue) {
+    return array_remove_back(queue);
+}
+
+void *queue_array_peek(const queue_array *queue) {
+    return array_node(queue, (int)queue->size - 1);
+}
+
+queue_list *queue_list_create() {
+    return dl_list_create();
+}
+
+queue_node *queue_node_create(void *data) {
+    return dl_node_create(data);
+}
+
+void queue_list_enqueue(queue_list *queue, queue_node *node) {
+    return dl_list_insert_front(queue, node);
+}
+
+queue_node *queue_list_dequeue(queue_list *queue) {
+    return dl_list_remove_back(queue);
+}
+
+queue_node *queue_list_peek(const queue_list *queue) {
+    return queue->tail;
+}
