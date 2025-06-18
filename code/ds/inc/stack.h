e64fdb6d13647021e0fff0cec7fb7c0660691e5f
caoaolong <eason105cc@163.com>
2025-06-18 18:21:13 +0800

    栈

---
+++ code/ds/inc/stack.h
@@ -0,0 +1,17 @@
+﻿//
+// Created by admin on 25-6-18.
+//
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <dl_list.h>
+
+typedef dl_list stack;
+typedef dl_list node;
+
+#define stack_push      dl_list_insert_back()
+#define stack_pop       dl_list_remove_back()
+#define stack_peek(V)   (V)->tail
+
+#endif //STACK_H
