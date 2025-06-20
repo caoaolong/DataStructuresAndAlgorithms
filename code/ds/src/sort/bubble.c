96aedb3f5111a4116b0948ae7a53b391d0b94dea
caoaolong <eason105cc@163.com>
2025-06-20 20:54:55 +0800

    冒泡排序

---
+++ code/ds/src/sort/bubble.c
@@ -0,0 +1,15 @@
+﻿//
+// Created by admin on 25-6-20.
+//
+
+void bubble_sort(int* arr, const int len) {
+    for (int i = 0; i < len; ++i) {
+        for (int j = 0; j < len - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                const int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
